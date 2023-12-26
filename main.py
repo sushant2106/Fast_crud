05c5db832a27509bd0fef179eaf902ce2cdbb5d5
sushant gupta <sushantgupta4u@gmail.com>
2023-12-26 22:49:58 +0530

    added put

--- main.py
+++ main.py
@@ -55,8 +55,12 @@ def  delete_blog(id:int,db:Session=Depends(get_db)):
 
 @app.put('/blog/{id}',status_code=status.HTTP_202_ACCEPTED)
 def update(id,request:schemas.Blog,db:Session=Depends(get_db)):
-    #db.query(models.Blog).filter(models.Blog.id==id).update(request)
-    db.query(models.Blog).filter(models.Blog.id==id).update(request)
+     #db.query(models.Blog).filter(models.Blog.id==id).update(request)
+    x=db.query(models.Blog).filter(models.Blog.id==id).first()
+    x.title=request.title
+    x.body=request.body
+    db.add(x)
+   
      
     db.commit()
     return 'updated Sucessfully'
